19485fc5bc0137bdf26803fce73efd06505b35bd
Kaspars <rezgalis.kaspars@gmail.com>
2019-03-25 15:20:22 +0200

    Update installer.py

--- installer.py
+++ installer.py
@@ -6,6 +6,10 @@ from subprocess import Popen, PIPE
 
 args = ["pkill" , "-f", "zigbee-mqtt-listener.py"]
 proc = Popen(args, stdout=PIPE, stderr=PIPE)
+out, err = proc.communicate()
+print(err)
 
 args = ["python" , "/home/pi/zigbee-mqtt-parser/zigbee-mqtt-listener.py"]
 proc = Popen(args, stdout=PIPE, stderr=PIPE)
+out, err = proc.communicate()
+print(err)
